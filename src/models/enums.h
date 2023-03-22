9e356a75b56367bba2dbac91dbe835845028b707
Leafoliage <leafoliages@gmail.com>
2023-03-22 23:32:01 +0800

    [add] enums

---
+++ src/models/enums.h
@@ -0,0 +1,12 @@
+#ifndef ENUM_H_INCLUDED
+#define ENUM_H_INCLUDED
+
+enum ObjectType { character, item };
+
+enum CharacterType { player, npc, ally, enemy };
+
+enum ItemType { weapon, armor, prop };
+
+enum WeaponType { melee, range };
+
+#endif // ENUM_H_INCLUDED
