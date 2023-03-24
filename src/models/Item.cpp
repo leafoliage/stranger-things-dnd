f8cc838594b374fcc00150f300d4eae33bf870ab
Leafoliage <leafoliages@gmail.com>
2023-03-24 22:03:26 +0800

    [add] Item implementation

--- src/models/Item.cpp
+++ src/models/Item.cpp
@@ -1,2 +1,39 @@
 #include "Item.h"
+#include "enums.h"
 
+Item::Item() {}
+
+Item::Item(string name, ItemType itemType, int hardness, int visibility):
+    Object(name, item), itemType(itemType), hardness(hardness), visibility(visibility) {}
+
+bool Item::triggerEvent(Object*) {
+    return true;
+}
+
+int Item::useHardness() {
+    return hardness;
+}
+
+void Item::setItemType(ItemType itemType) {
+    this->itemType = itemType;
+}
+
+void Item::setHardness(int hardness) {
+    this->hardness = hardness;
+}
+
+void Item::setVisibility(int visibility) {
+    this->visibility = visibility;
+}
+
+ItemType Item::getItemType() const {
+    return itemType;
+}
+
+int Item::getHardness() const {
+    return hardness;
+}
+
+int Item::getVisibility() const {
+    return visibility;
+}
